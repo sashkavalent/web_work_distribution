ff1db3275c4dbe182368103f19ae0f62d52ec59a
sasha <sashkavalent@gmail.com>
2014-06-17 22:16:51 +0300

    First version.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def upload
+    name = params['xlsx'].original_filename
+    directory = 'xlsx_files'
+    path = File.join(directory, name)
+    binding.pry
+    File.open(path, "wb") { |file| file.write(params['xlsx'].read) }
+    render action: :index
+  end
+end
